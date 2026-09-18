2b57f6c5ecc8b862f10491d2cbfdb9db3b71c35d
gim_z66 <gimholg1992@foxmail.com>
2026-09-18 10:37:43 +0800

    PlayerScorePlayerName 简化名显示

--- src/LFW/ui/component/PlayerScorePlayerName.ts
+++ src/LFW/ui/component/PlayerScorePlayerName.ts
@@ -3,26 +3,22 @@ import type { IStyle } from '../../defines/IStyle';
 import { TeamEnum } from '../../defines/TeamEnum';
 import { Defines } from '../../defines/defines';
 import { TextInfo } from "../../ditto/image/TextInfo";
-import { Entity } from '../../entity/Entity';
-import { is_bot_ctrl, is_human_ctrl } from '../../entity/type_check';
 import { PlayerScore } from "./PlayerScore";
 import { UIComponent } from "./UIComponent";
 
-
+/**
+ * Note: 
+ *    你的这些组件是怎么想的... 
+ *    以后最好避组件间免层层依赖的情况
+ *      -Gim
+ */
 export class PlayerScorePlayerName extends UIComponent {
   static override readonly TAGS: string[] = ["PlayerScorePlayerName"];
-  private fighter?: Entity;
 
   override on_show(): void {
-    const fighter = this.fighter = this.node.lookup_component(PlayerScore)?.fighter;
-    let name: string = '-';
-    if (fighter) {
-      const { ctrl } = fighter;
-      if (is_human_ctrl(ctrl) || is_bot_ctrl(ctrl)) {
-        name = ctrl.player?.name || '-';
-      }
-    }
-    const team = this.fighter?.team;
+    const fighter = this.node.lookup_component(PlayerScore)?.fighter;
+    const text: string = fighter?.name ?? '-';
+    const team = fighter?.team;
     const team_info = Defines.TeamInfoMap[team as TeamEnum] ?? Defines.TeamInfoMap[TeamEnum.Independent]
     Object.assign(this.node, {
       outlineColor: team_info.txt_outline_color,
@@ -33,6 +29,6 @@ export class PlayerScorePlayerName extends UIComponent {
       font: "12px Arial",
       fill_style: team_info.txt_color
     };
-    this.node.text = new TextInfo({ text: name, style });
+    this.node.text = new TextInfo({ text, style });
   }
 }
