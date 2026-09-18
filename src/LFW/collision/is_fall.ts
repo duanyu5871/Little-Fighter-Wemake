2eb36fac64a094f1c7915beb3950b1fccf8ce768
gim <gimholg1992@foxmail.com>
2026-09-19 06:50:49 +0800

    空中角色更易被击倒

--- src/LFW/collision/is_fall.ts
+++ src/LFW/collision/is_fall.ts
@@ -2,7 +2,7 @@ import type { Collision } from "./Collision";
 import { Defines as D, StateEnum as SE } from "../defines";
 import { is_fighter } from "../entity/type_check";
 
-export function is_fall(collision: Collision) {
+export function is_fall(collision: Collision): boolean {
   const { victim } = collision;
   return (
     !is_fighter(victim) ||
@@ -10,6 +10,6 @@ export function is_fall(collision: Collision) {
     victim.hp <= 0 ||
     victim.state === SE.Frozen ||
     (victim.fall_value < D.DEFAULT_FALL_VALUE_DIZZY && SE.Caught === victim.state) ||
-    (victim.fall_value <= D.DEFAULT_FALL_VALUE_DIZZY && victim.position.y > victim.ground_y)
+    (victim.fall_value <= D.DEFAULT_FALL_VALUE_CRITICAL && !victim.is_on_ground)
   );
 }
